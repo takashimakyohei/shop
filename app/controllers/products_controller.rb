ea7961946e3787debcc72a8680080638e5e921ca
”高島恭平” <hagi.suzu@icloud.com>
2019-08-04 12:19:58 +0000

    changed_products_index,new

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,30 @@
+class ProductsController < ApplicationController
+  def index
+    @products =Product.all
+  end
+  
+  def new
+    @product =Product.new
+  end
+  
+  def create
+    @product = current_user.products.new(product_params)
+  if @product.save
+    redirect_to products_path,success:"商品登録に成功しました"
+  else
+    flash.now[:danger]="商品登録に失敗しました"
+    render :new
+  end
+  
+  def show
+    
+  end
+    
+  end
+  
+  private
+  def product_params
+    params.require(:product).permit(:title,:image,:description,:price)
+  end
+  
+end
